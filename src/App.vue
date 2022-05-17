c16c30a36be6b318c2098fe5b2c08ae7795b7436
typeng <typeng@live.cn>
2022-05-17 23:08:45 +0800

    使用sync实现父子组件同步通信

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <h1>v-model深入</h1>
+    <h3>{{ msg }}</h3>
     <Test v-model="msg"></Test>
+    <hr />
+    <h1>不使用sync修饰符</h1>
+    <h3>小李还剩{{ money }}元</h3>
+    <XiaoMing :money="money" @update:money="money = $event"></XiaoMing>
+    <hr />
+    <h1>使用sync修饰符</h1>
+    <h3>小李还剩{{ money }}元</h3>
+    <XiaoMing2 :money.sync="money"></XiaoMing2>
   </div>
 </template>
 
 <script>
-  import Test from './components/test.vue'
+  import Test from './components/Test'
+  import XiaoMing from './components/XiaoMing'
+  import XiaoMing2 from './components/XiaoMing2'
   export default {
     name: 'App',
     data() {
       return {
-        msg: 'hello'
+        msg: 'hello',
+        money: 10000
       }
     },
-    components: { Test }
+    components: { Test, XiaoMing, XiaoMing2 }
   }
 </script>
---
+++ src/components/XiaoMing2.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>小明每次<button @click="$emit('update:money', money - 100)">花</button>小李100元</div>
+</template>
+
+<script>
+  export default {
+    name: 'XiaoMing2',
+    props: ['money']
+  }
+</script>
+
+<style>
+</style>
